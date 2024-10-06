4c7f122af05335efaa996a018f00b6df0efa66ff
Jan Gazda <7480694+1oglop1@users.noreply.github.com>
2024-10-06 10:48:35 +0200

    repro 13

--- el_app/mix.exs
+++ el_app/mix.exs
@@ -22,8 +22,9 @@ defmodule ElApp.MixProject do
     [
       {:lib_pkg, git: "https://github.com/jans-forks/elixir_lib", tag: "v0.1.0", app: false},
       {:git_lib_pkg, git: "https://github.com/jans-forks/elixir_lib", tag: "v0.2.0", app: false},
-      {:ref_lib_pkg, git: "https://github.com/jans-forks/elixir_lib", ref: "4888c25b27e47369296ebd66502a3d3ec6e955db", tag: "v0.1.0",app: false},
-      {:ref_lib_tag, git: "https://github.com/jans-forks/elixir_lib", ref: "v0.1.0", app: false}
+      {:ref_lib_pkg, git: "https://github.com/jans-forks/elixir_lib", ref: "4888c25b27e47369296ebd66502a3d3ec6e955db",app: false},
+      {:ref_lib_tag, git: "https://github.com/jans-forks/elixir_lib", ref: "v0.1.0", app: false},
+      {:ref_lib_branch, git: "https://github.com/jans-forks/elixir_lib", ref: "main", app: false}
     ]
   end
 end
